befe581b22971fa71a6ca5448659ef06810721a3
ilya <sushkov.ilia@gmail.com>
2018-06-12 02:28:16 +0300

    deploy js-game

--- js-game.php
+++ js-game.php
@@ -2,12 +2,9 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Simple JS-game</title>
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/project.css">
-    <link rel="shortcut icon" href="icon/favicon.png" type="image/png">
+    <link rel='stylesheet' href='css/js-game.css'>
+    <link rel='shortcut icon' href='js-game/img/house.png' type='image/png'>
 </head>
 <body>
 <div class="wrapper">
@@ -16,19 +13,19 @@
             <div class="header2">
                 <div class="headerLeft">
                     <a href="index.html">
-                        <h2 class="ilyaSushkov">ILYA SUSHKOV</h2>
+                        <p class="name">ILYA SUSHKOV</p>
                     </a>
-                    <p class="artist">3D Artist</p>
+                    <p class="artist">Developer & Artist</p>
                 </div>
                 <div class="headerRight">
                     <div class="headerRightItem">
                         <a href="me.html">
-                            <h2 class="linkHeader">ABOUT ME</h2>
+                            <p class="linkHeader">ABOUT ME</p>
                         </a>
                     </div>
                     <div class="headerRightItem">
                         <a href="index.html">
-                            <h2 class="linkHeader">HOME</h2>
+                            <p class="linkHeader">HOME</p>
                         </a>
                     </div>
                 </div>
@@ -36,96 +33,36 @@
         </header>
         <div class="projectContent">
             <div class="gallery">
-                <div class="galleryLeft">
-                    <a href="venomSnake.html">
-                        <div class="galleryLinkLeft">
-                            <h1 class="indexLeft"></h1>
-                        </div>
-                    </a>
-                </div>
-                <div class="projectBlock">
-                    <h2 class="projectName">Simple JS-game</h2>
-                    <div class="wrap">
-                        <input type="radio" name="slides" id="point1" checked>
-                        <input type="radio" name="slides" id="point2">
-                        <input type="radio" name="slides" id="point3">
-                        <div class="slider">
-                            <figure class="slides auto1 projectBlock2">
-                                <img class="projectImg" src="img/versus1.JPG" alt="Prompto Argentum 1">
-                                <figcaption>
-                                    <a href="img/versus1.JPG"></a>
-                                </figcaption>
-                            </figure>
-                            <figure class="slides auto2 projectBlock2">
-                                <img class="projectImg" src="img/versus2.JPG" alt="Prompto Argentum 2">
-                                <figcaption>
-                                    <a href="img/versus2.JPG"></a>
-                                </figcaption>
-                            </figure>
-                            <figure class="slides auto3 projectBlock2">
-                                <img class="projectImg" src="img/versus3.JPG" alt="Prompto Argentum 3">
-                                <figcaption>
-                                    <a href="img/versus3.JPG"></a>
-                                </figcaption>
-                            </figure>
-                        </div>
-                        <div class="control">
-                            <label for="point1">
-                                <img class="labelImg" src="img/versus1.JPG" alt="1">
-                            </label>
-                            <label for="point2">
-                                <img class="labelImg" src="img/versus2.JPG" alt="2">
-                            </label>
-                            <label for="point3">
-                                <img class="labelImg" src="img/versus3.JPG" alt="3">
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <div class="galleryRight">
-                    <a href="noahs-ark.html">
-                        <div class="galleryLinkRight">
-                            <h1 class="indexRight">></h1>
-                        </div>
-                    </a>
-                </div>
+                <p class="project-name">SIMPLE JS-GAME</p>
+                <img class="js-img" src="img/js-game.jpg" alt="js-game image">
             </div>
             <div class="descriptionBlock">
-                <div class="descriptionBlockItem1"><p class="description"><a
-                        href="https://sketchfab.com/models/59457f68e10047a098eb681ceb4a04f3">View 3D model</a> on
-                    sketchfab.</p></div>
-                <div class="descriptionBlockItem1">
-                    <h1 class="descriptionBlockName">TOOLS</h1>
-                    <div class="tools">
-                        <div class="toolsItem">
-                            <div class="toolsIcon">
-                                <img src="icon/3dMaxLogo.png" alt="3dMax">
-                            </div>
-                            <p class="toolsName">3D MAX</p>
-                        </div>
-                        <div class="toolsItem">
-                            <div class="toolsIcon">
-                                <img src="icon/modoLogo.png" alt="Modo">
-                            </div>
-                            <p class="toolsName">MODO</p>
-                        </div>
+                <div class="descriptionBlockItem2">
+                    <h1 class="descriptionBlockName">ОПИСАНИЕ</h1>
+                    <p class="description">
+                        Примитивная стратегия.<br>Цель проекта: 
+                        <span class='emphasis'>демонстрация базовых знаний JavaScript</span>
+                        без использования MVC, JQuery, фреймворков и тп. Игра не требует установки.
+                    </p>
+                    <div class='js-notice'>
+                        <h1 class="js-notice-title">ВНИМАНИЕ</h1>
+                        <p class="js-notice-description">
+                            Отсутсвует адаптивная верстка.
+                            Игра нормально функционирует <span class='js-notice-emphasis'>только на компьютере</span>,
+                            с разрешением экрана <span class='js-notice-emphasis'>не менее 1000 px</span>, иначе все посыпится :)
+                        </p>
                     </div>
                 </div>
-                <div class="descriptionBlockItem2">
-                    <h1 class="descriptionBlockName">DESCRIPTION</h1>
-                    <p class="description">Сosplay from FFantasy Versus XIII weapons for 3D printing <a
-                            href="https://www.google.ru/search?newwindow=1&q=ff+%E3%83%B4%E3%82%A7%E3%83%AB%E3%82%B5%E3%82%B9+13&tbm=isch&tbs=simg:CAQSkwEJ7G4-BTsjjo0ahwELEKjU2AQaAAwLELCMpwgaYgpgCAMSKOsbkhWTFeobkxybBeUb4RvoG4scwC3QL6oltC-1L7kv5iTPL8gt0S8aMCZ0R38b-jTyvsGOscNkZznsTzjm0KWuton48NEZzWfz534vGKcS3nlpE4zmAOUxPSAEDAsQjq7-CBoKCggIARIENSUZFww&sa=X&ved=0ahUKEwiXgc63-uHUAhUDD5oKHTCNB4cQ2A4IJCgB&biw=1920&bih=984">Shotgun
-                        Guy</a>.
-                    </p>
-                </div>
                 <div class="empty"></div>
+            </div>
+            <div class='wrapper-js-play'>
+                <a class='js-play' href='./js-game/index.php'>PLAY GAME</a>
             </div>
         </div>
     </div>
     <div class="footer">
         <div class="footerCenter">
-            <p class="update"><span class="update2">\\\\\\\\\\\\\\\\\\ </span>UPDATE: 12.07.2017<span
-                    class="update2"> /////////////////</span></p>
+            <p class="update">Last update: 11-06-2018</p>
         </div>
     </div>
 </div>
