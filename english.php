0c075333bdb4aac1882c4c89606566d257881d08
isushkov <sushkov@protonmail.com>
2018-09-01 17:35:50 +0300

    +

--- english.php
+++ english.php
@@ -7,25 +7,8 @@
     <link rel='shortcut icon' href='js-game/img/house.png' type='image/png'>
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/project.css">
-    <script>
-        (function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function () {
-                        (i[r].q = i[r].q || []).push(arguments)
-                    }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                    m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-        ga('create', 'UA-101911065-1', 'auto');
-        ga('send', 'pageview');
-
-    </script>
+    <link rel='stylesheet' href='css/english.css'>
     <title>English language tests</title>
-    <link rel='stylesheet' href='css/js-game.css'>
 </head>
 <body>
 <div class="wrapper">
@@ -54,38 +37,28 @@
         </header>
         <div class="projectContent">
             <div class="gallery">
-                <div class="galleryLeft">
-                    <a href="inception.html">
-                        <div class="galleryLinkLeft">
-                            <h1 class="indexLeft"><</h1>
-                        </div>
-                    </a>
-                </div>
                 <div class="content-gallery">
-                    <p class="project-name">Тесты на знание английской лексики</p>
-                    <div class="descriptionBlockEnglish">
-                        <h1 class="descriptionBlockName">Описание</h1>
-                        <p class="description">
-                            Сайт содержит набор тестов, у умным побором вопросов
-                        </p>
-                        <div class='wrapper-js-play'>
-                            <a class='js-play' href='eng/index.php'>Учить английскую лексику</a>
-                        </div>
+                    <p class="project-name">Тест на знание английской лексики</p>
+                    <div class="description">
+                        <img class="projectImg" src="img/eng.png">
+                        Тест на знание английской лексики с умной системой подбора вопросов
+                        <a class='href' href='eng/home.php'>Пройти на сайт</a>
                     </div>
                 </div>
-            <div class="galleryRight">
-                <a href="js-game.php">
-                    <div class="galleryLinkRight">
-                        <h1 class="indexRight">></h1>
-                    </div>
-                </a>
             </div>
+            <div class="navigation">
+                <a href="inception.html">
+                    <h1 class="href2">< Туда</h1>
+                </a>
+                <a href="js-game.php">
+                    <h1 class="href2">Cюда ></h1>
+                </a>
             </div>
         </div>
     </div>
     <div class="footer">
         <div class="footerCenter">
-            <p class="update">Last update: 17-08-2018</p>
+            <p class="update">Last update: 01-09-2018</p>
         </div>
     </div>
 </div>
