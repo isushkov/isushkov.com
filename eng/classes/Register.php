e2a857fc525ac2a7529afab5b39cffb9051efc9b
isushkov <sushkov@protonmail.com>
2018-09-08 22:27:41 +0300

    +

--- eng/classes/Register.php
+++ eng/classes/Register.php
@@ -10,51 +10,51 @@ class Register extends App
     public function register()
     {
         if (isset($_POST['submit'])) {
+            $login = $_POST['login'];
+            $postPassword = $_POST['password'];
+
             $this->errors = array();
             // проверям логин
-            if (strlen($_POST['login']) < 3 || strlen($_POST['login']) > 30) {
+            if (strlen($login) < 3 || strlen($login) > 30) {
                 $this->errors[] = "Логин должен быть от 3-х до 30 символов";
-            } else if (!preg_match("/^[a-zA-Z0-9]+$/",$_POST['login'])) {
+            } else if (!preg_match("/^[a-zA-Z0-9]+$/",$login)) {
                 $this->errors[] = "Логин может состоять только из букв английского алфавита и цифр";
             }
-            if (strlen($_POST['password']) < 3 || strlen($_POST['password']) > 30) {
+            if (strlen($postPassword) < 3 || strlen($postPassword) > 30) {
                 $this->errors[] = "Пароль должен быть от 3-х до 30 символов";
             }
-            if ($_POST['password'] !== $_POST['check-password']) {
+            if ($postPassword !== $_POST['check-password']) {
                 $this->errors[] = "Пароли не совпадают";
             }
             // Соединямся с БД
             $dbh = $this->getConnection();
             // проверяем, не сущестует ли пользователя с таким именем
-            $sth = $dbh->prepare("select id from users where login = '".$_POST['login']."'");
+            $sth = $dbh->prepare("select count(*) from users where login = '$login'");
             $sth->execute();
-            $userCount = count($sth->fetchAll(PDO::FETCH_COLUMN));
+            $userCount = $sth->fetch(PDO::FETCH_COLUMN);
             if ($userCount > 0) {
                 $this->errors[] = "Пользователь с таким логином уже существует";
             }
             if (count($this->errors) == 0) {
-                $login = $_POST['login'];
                 // Убераем лишние пробелы и делаем шифрование
-                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $password = password_hash($postPassword, PASSWORD_DEFAULT);
                 // добавляем в БД нового пользователя
                 $sth = $dbh->prepare("insert into users set login = '$login', password = '$password'");
                 $sth->execute();
                 //add message
                 $_SESSION['new_user'] = 1;
-
                 // сразу авторизовать
-                // Генерируем случайное число и шифруем его
-                $hash = password_hash($this->random_str(10), PASSWORD_DEFAULT);
                 // Записываем в БД новый хеш авторизации
-                $sth = $dbh->prepare("select id, password from users where login = '".$_POST['login']."' LIMIT 1");
+                $hash = password_hash($this->random_str(10), PASSWORD_DEFAULT);
+                // get user_id
+                $sth = $dbh->prepare("select id from users where login = '$login'");
                 $sth->execute();
-                $userData = $sth->fetchAll(PDO::FETCH_ASSOC);
-                $sth = $dbh->prepare("UPDATE users set hash = '$hash' WHERE id = '".$userData[0]['id']."'");
+                $userId = $sth->fetch(PDO::FETCH_COLUMN);
+                $sth = $dbh->prepare("UPDATE users set hash = '$hash' WHERE id = '$userId'");
                 $sth->execute();
                 // Ставим куки
                 setcookie('hash', $hash, time() + 60*60*24*30);
-                // setcookie('user_id', $userData['id'], time() + 60*60*24*30);
-                $_SESSION['user_id'] = $userData[0]['id'];
+                $_SESSION['user_id'] = $userId;
                 header("Location: profile.php");
                 exit();
             }
